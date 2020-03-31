da4d8826ed9d4920e9e7d684cac9fb5820187a5d
UgurEgemenKaya <uegemenkaya93@gmail.com>
2020-03-31 14:33:09 +0300

    ilerleme_001

--- Kutuphane_Proje/settings.py
+++ Kutuphane_Proje/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'books.apps.BooksConfig',
     'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
