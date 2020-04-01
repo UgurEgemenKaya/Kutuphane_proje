345829abd68eb01da440b1c4eb5c65379f0c11c0
UgurEgemenKaya <uegemenkaya93@gmail.com>
2020-04-01 17:09:20 +0300

    ilerleme_002

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from books.models import Category
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+admin.site.register(Category, CategoryAdmin)
