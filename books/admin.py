dc8179a1c29a5d03cfaee877d47854abbd8c4203
UgurEgemenKaya <uegemenkaya93@gmail.com>
2020-04-05 16:54:59 +0300

    ilerleme_003

--- books/admin.py
+++ books/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
-from books.models import Category
+from books.models import Category, Product
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','category','amount', 'status']
+    list_filter = ['status','category']
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- books/models.py
+++ books/models.py
@@ -19,5 +19,26 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    amount = models.IntegerField()
+    detail = models.TextField()
+    status = models.CharField(max_length=10,choices=STATUS)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
 
 # Create your models here.
