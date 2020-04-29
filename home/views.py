d2402d58c2f8d7d9c866f16eb00573b3371f8860
UgurEgemenKaya <uegemenkaya93@gmail.com>
2020-04-29 16:34:21 +0300

    ilerleme_010

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
-from home.models import Setting
+from books.models import Category
+from home.models import Setting , ContactFormu, ContactFormMessage
 
 
 def index(request):
@@ -21,6 +22,22 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız başarı ile gönderilmiştir.  Teşekkür ederiz ")
+            return HttpResponseRedirect ('/iletisim')
+
     setting = Setting.objects.get(pk=1)
-    context = {'setting': setting, 'page':'hakkimizda'}
+    form = ContactFormu
+    category = Category.objects.all()
+    context = {'setting': setting,'category': category, 'form': form}
     return render(request, 'iletisim.html', context)
